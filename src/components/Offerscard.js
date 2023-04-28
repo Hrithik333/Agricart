330ffa0a5d8bc9d5e98750cdaf31481828f42c59
Hrithik333 <67138916+Hrithik333@users.noreply.github.com>
2023-04-28 14:23:05 +0530

    Add files via upload

---
+++ src/components/Offerscard.js
@@ -0,0 +1,38 @@
+  
+  import React from 'react'
+  
+  const Offerscard = () => {
+      return (
+        <div className="box">
+            <div className="container">
+              <div className="row">   
+     
+                    <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div className="box-part text-center">
+                            <img src="https://cdn1.desidime.com/topics/photos/1533381/original/Standard-Chartered-TCPageMWebDomestic.jpg?1674828092" alt="sc" width="300" height="250"/>
+
+                            <div className="title">
+                                <h4>SAC_15</h4>
+                            </div>
+                            
+                            <div className="offers-card-text">
+                                <span>Get 15% discount using Standard Chartered Bank Cards.</span>
+                                <p>Use code SAC_15 & get 15% discount up to Rs.100 on orders above Rs.500</p>
+                            </div>
+                            
+                            <a href="/more">Learn More</a>
+                            
+                        </div>
+                        
+                    </div>	
+              </div>
+             </div>
+        </div>
+          
+      )
+  }
+  
+  export default Offerscard
+  
+  
+  
